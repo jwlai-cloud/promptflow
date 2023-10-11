12bb083795efa654a7b0e42945e76147cd137d62
Sourcery AI <>
2023-10-11 09:45:45 +0000

    'Refactored by Sourcery'

--- src/promptflow/promptflow/contracts/flow.py
+++ src/promptflow/promptflow/contracts/flow.py
@@ -119,10 +119,7 @@ class FlowInputAssignment(InputAssignment):
         :return: Whether the input value is a flow input.
         :rtype: bool
         """
-        for prefix in FLOW_INPUT_PREFIXES:
-            if input_value.startswith(prefix):
-                return True
-        return False
+        return any(input_value.startswith(prefix) for prefix in FLOW_INPUT_PREFIXES)
 
     @staticmethod
     def deserialize(value: str) -> "FlowInputAssignment":
@@ -190,11 +187,10 @@ class ActivateCondition:
         :return: The activate condition constructed from the dict.
         :rtype: ActivateCondition
         """
-        result = ActivateCondition(
+        return ActivateCondition(
             condition=InputAssignment.deserialize(data["when"]),
             condition_value=data["is"],
         )
-        return result
 
 
 @dataclass
@@ -214,12 +210,11 @@ class SkipCondition:
         :return: The skip condition constructed from the dict.
         :rtype: SkipCondition
         """
-        result = SkipCondition(
+        return SkipCondition(
             condition=InputAssignment.deserialize(data["when"]),
             condition_value=data["is"],
             return_value=InputAssignment.deserialize(data["return"]),
         )
-        return result
 
 
 @dataclass
@@ -304,8 +299,7 @@ class FlowInputDefinition:
     is_chat_history: bool = None
 
     def serialize(self):
-        data = {}
-        data["type"] = self.type.value
+        data = {"type": self.type.value}
         if self.default:
             data["default"] = str(self.default)
         if self.description:
@@ -349,8 +343,7 @@ class FlowOutputDefinition:
 
     def serialize(self):
         """Serialize the flow output definition to a dict."""
-        data = {}
-        data["type"] = self.type.value
+        data = {"type": self.type.value}
         if self.reference:
             data["reference"] = self.reference.serialize()
         if self.description:
@@ -417,9 +410,10 @@ class NodeVariants:
         :return: The node variants constructed from the dict.
         :rtype: NodeVariants
         """
-        variants = {}
-        for variant_id, node in data["variants"].items():
-            variants[variant_id] = NodeVariant.deserialize(node)
+        variants = {
+            variant_id: NodeVariant.deserialize(node)
+            for variant_id, node in data["variants"].items()
+        }
         return NodeVariants(default_variant_id=data.get("default_variant_id", ""), variants=variants)
 
 
@@ -437,7 +431,7 @@ class Flow:
 
     def serialize(self):
         """Serialize the flow to a dict."""
-        data = {
+        return {
             "id": self.id,
             "name": self.name,
             "nodes": [n.serialize() for n in self.nodes],
@@ -445,7 +439,6 @@ class Flow:
             "outputs": {name: o.serialize() for name, o in self.outputs.items()},
             "tools": [serialize(t) for t in self.tools],
         }
-        return data
 
     @staticmethod
     def _import_requisites(tools, nodes):
@@ -648,10 +641,11 @@ class Flow:
             if node.connection:
                 connection_names.add(node.connection)
                 continue
-            if node.type == ToolType.PROMPT or node.type == ToolType.LLM:
+            if node.type in [ToolType.PROMPT, ToolType.LLM]:
                 continue
-            tool = self.get_tool(node.tool) or self._tool_loader.load_tool_for_node(node)
-            if tool:
+            if tool := self.get_tool(
+                node.tool
+            ) or self._tool_loader.load_tool_for_node(node):
                 connection_names.update(self._get_connection_name_from_tool(tool, node).values())
         return set({item for item in connection_names if item})
 
@@ -663,8 +657,9 @@ class Flow:
         # Ignore Prompt node and LLM node, due to they do not have connection inputs.
         if not node or node.type == ToolType.PROMPT or node.type == ToolType.LLM:
             return []
-        tool = self.get_tool(node.tool) or self._tool_loader.load_tool_for_node(node)
-        if tool:
+        if tool := self.get_tool(
+            node.tool
+        ) or self._tool_loader.load_tool_for_node(node):
             return list(self._get_connection_name_from_tool(tool, node).keys())
         return []
 
